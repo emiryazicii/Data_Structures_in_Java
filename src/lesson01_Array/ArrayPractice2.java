74bd287e5c6ed7fc20e07e917e649d0df1f64239
European01 <emiryaziciofficial@gmail.com>
2024-01-23 21:01:09 -0500

    Add an example to practice.

--- src/lesson01_Array/ArrayPractice2.java
+++ src/lesson01_Array/ArrayPractice2.java
@@ -1,4 +1,27 @@
 package lesson01_Array;
 
+import java.util.Arrays;
+
 public class ArrayPractice2 {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = new int[100];
+
+        for (int i = 0,j = 1; i < arr1.length; i++, j++) {
+            arr1[i] = j;
+
+        }
+        System.out.println(Arrays.toString(arr1));
+
+        System.out.println("-----------------------------------------------------------------------");
+
+        int[] arr2 = new int[100];
+        for (int i = 0, j= 100; i < arr2.length; i++, j--) {
+
+            arr2[i] = j;
+        }
+        System.out.print(Arrays.toString(arr2));
+
+    }
 }
