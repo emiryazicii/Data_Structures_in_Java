fc7604439e667cca142eb3642cdf77d5592009b8
European01 <emiryaziciofficial@gmail.com>
2024-01-23 23:07:06 -0500

    Add an example to practice.

--- src/lesson01_Array/MaxNumberInArray.java
+++ src/lesson01_Array/MaxNumberInArray.java
@@ -1,4 +1,19 @@
 package lesson01_Array;
 
 public class MaxNumberInArray {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {100,20,500,40,-10,30};
+
+        int max = numbers[0]; // assume that first element is the maximum number
+
+        for (int i = 1; i < numbers.length; i++) {
+
+            if(numbers[i] > max){ // compares the element of the array with current maximum number
+                max = numbers[i]; // replace the current maximum number
+            }
+        }
+        System.out.println("max = " + max);
+    }
 }
