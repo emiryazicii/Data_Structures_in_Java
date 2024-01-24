00d36a8118a9d89aee2fa566655ade009f4a65d6
European01 <emiryaziciofficial@gmail.com>
2024-01-23 23:27:24 -0500

    Add requirements.

--- src/lesson01_Array/ReverseAnArray.java
+++ src/lesson01_Array/ReverseAnArray.java
@@ -35,3 +35,12 @@ public class ReverseAnArray {
 
     }
 }
+/*
+ Write a program that can reverse an array of integers and returns it as new array
+	        ex:
+	            array = {1,2,3,4,5};
+
+	        output:
+	            reversedArray = {5,4,3,2,1};
+
+ */
