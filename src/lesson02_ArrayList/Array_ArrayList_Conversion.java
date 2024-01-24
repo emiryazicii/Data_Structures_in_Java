96fa9e2c11bd32cad7f59b0003532a8ab1dba8ab
European01 <emiryaziciofficial@gmail.com>
2024-01-24 00:15:30 -0500

    Add some examples to practice.

--- src/lesson02_ArrayList/Array_ArrayList_Conversion.java
+++ src/lesson02_ArrayList/Array_ArrayList_Conversion.java
@@ -1,4 +1,26 @@
 package lesson02_ArrayList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Array_ArrayList_Conversion {
+
+    public static void main(String[] args) {
+
+
+        String[] arr = {"A","B","C","D"};
+
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(arr));
+
+        System.out.println(list);
+
+        System.out.println("------------------------------------------------------------------------------------");
+
+
+        ArrayList<String> list2 = new ArrayList<>(Arrays.asList("Java","Python","C#"));
+
+        String[] language = list2.toArray(new String[0]);
+
+        System.out.println(Arrays.toString(language));
+    }
 }
