8f699fce0aaa47872b85e263307028c0a4e5d534
European01 <emiryaziciofficial@gmail.com>
2024-01-23 23:08:00 -0500

    Add an example to practice.

--- src/lesson01_Array/MinNumberInArray.java
+++ src/lesson01_Array/MinNumberInArray.java
@@ -1,4 +1,19 @@
 package lesson01_Array;
 
 public class MinNumberInArray {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {100,20,500,40,-10,30};
+
+        int min = numbers[0]; // assume that first element is the minimum number
+
+        for (int i = 1; i < numbers.length; i++) {
+
+            if(numbers[i] < min){ // compares the element of the array with current minimum number
+                min = numbers[i]; // replace the current minimum number
+            }
+        }
+        System.out.println("min = " + min);
+    }
 }
