76d917f5782bcecbb97372edf66cedb903aace1f
European01 <emiryaziciofficial@gmail.com>
2024-01-24 00:08:29 -0500

    Add an example to practice.

--- src/lesson02_ArrayList/IteratingArrayList.java
+++ src/lesson02_ArrayList/IteratingArrayList.java
@@ -1,4 +1,31 @@
 package lesson02_ArrayList;
 
+import java.util.ArrayList;
+
 public class IteratingArrayList {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(100);
+        numbers.add(200);
+        numbers.add(300);
+        numbers.add(400);
+        numbers.add(500);
+        numbers.add(3,255);
+
+        //numbers.add(300,2000);
+
+        System.out.println(numbers);
+
+        for (Integer each : numbers) {
+            System.out.print(each+" ");
+        }
+        System.out.println();
+        System.out.println("----------------------------------------------------------------------------");
+        for (int i = numbers.size() - 1; i >= 0; i--) {
+            System.out.print(numbers.get(i)+" ");
+        }
+    }
 }
