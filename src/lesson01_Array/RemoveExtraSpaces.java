fe33283ed340adaea10c39440667e602223d8d3c
European01 <emiryaziciofficial@gmail.com>
2024-01-23 23:46:00 -0500

    Add an example to practice.

--- src/lesson01_Array/RemoveExtraSpaces.java
+++ src/lesson01_Array/RemoveExtraSpaces.java
@@ -1,4 +1,31 @@
 package lesson01_Array;
 
+import java.util.Arrays;
+
 public class RemoveExtraSpaces {
+
+    public static void main(String[] args) {
+
+        String str = "    Hello world          I       love     java     ";
+
+        String[] words = str.split(" ");
+        System.out.println(Arrays.toString(words));
+
+        str = "";
+        for (String each : words) {
+            if (!each.isEmpty()) {
+                str += each + " ";
+            }
+        }
+        str = str.trim();
+        System.out.println(str);
+    }
 }
+/*
+1. Write a program that can remove all the extra space from string
+			Ex:
+				str = "  Hello world      I      love      Java    "
+
+        	Output:
+        		Hello world I love Java
+ */
