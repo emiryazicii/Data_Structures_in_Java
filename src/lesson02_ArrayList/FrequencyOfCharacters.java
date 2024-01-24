6bab4e32bbb1acba10a03ed83c3b2cc94afc17a3
European01 <emiryaziciofficial@gmail.com>
2024-01-24 00:43:00 -0500

    Add an example to practice.

--- src/lesson02_ArrayList/FrequencyOfCharacters.java
+++ src/lesson02_ArrayList/FrequencyOfCharacters.java
@@ -1,4 +1,24 @@
 package lesson02_ArrayList;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class FrequencyOfCharacters {
+
+    public static void main(String[] args) {
+
+        String str = "aaabbbbccccc";
+
+        String result = ""; // a3b4c5
+
+        for (String each : str.split("")) {
+
+            int frequency = Collections.frequency(Arrays.asList(str.split("")), each);
+
+            if (!result.contains(each)) {
+                result += each + frequency;
+            }
+        }
+        System.out.println(result);
+    }
 }
