0ae2b4d5ed7dd4a556b824676768288fec97b583
European01 <emiryaziciofficial@gmail.com>
2024-01-30 11:03:33 -0500

    Add an example to practice.

--- src/lesson04_Maps/MapPractice6.java
+++ src/lesson04_Maps/MapPractice6.java
@@ -1,4 +1,20 @@
 package lesson04_Maps;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class MapPractice6 {
+
+    public static void main(String[] args) {
+
+        Map<String, String> map = new TreeMap<>();
+
+        map.put("C02","Josh");
+        map.put("B03","Nora");
+        map.put("A02","Khashayar");
+        map.put("D02","Abidullah");
+        map.put("A03","Tatiana");
+
+        map.forEach((k,v) -> System.out.println(k+" : "+v));
+    }
 }
