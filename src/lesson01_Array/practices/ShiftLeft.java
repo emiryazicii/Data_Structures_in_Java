94174bce5dbde8808237f3dda841f430797f20d7
European01 <emiryaziciofficial@gmail.com>
2024-02-05 22:37:24 -0500

    Add an example to practice.

--- src/lesson01_Array/practices/ShiftLeft.java
+++ src/lesson01_Array/practices/ShiftLeft.java
@@ -1,4 +1,20 @@
 package lesson01_Array.practices;
 
+import java.util.Arrays;
+
 public class ShiftLeft {
+
+    public static void main(String[] args) {
+
+        int[] array = {10,20,30,40,50};
+
+        int[] shiftedArray = new int[array.length];
+        shiftedArray[shiftedArray.length-1] = array[0];
+
+        for (int i = 0; i < shiftedArray.length-1; i++) {
+
+            shiftedArray[i] = array[i+1];
+        }
+        System.out.println(Arrays.toString(shiftedArray));
+    }
 }
