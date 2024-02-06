8f29386dd32c1aa4c361499c7e80d493e8a28376
European01 <emiryaziciofficial@gmail.com>
2024-02-05 22:33:03 -0500

    Add an example to practice.

--- src/lesson01_Array/practices/BeginningEnd.java
+++ src/lesson01_Array/practices/BeginningEnd.java
@@ -1,4 +1,25 @@
 package lesson01_Array.practices;
 
+import java.util.Arrays;
+
 public class BeginningEnd {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Mukerrem", "Aurelio", "Huseyin", "Necla", "Oswaldo", "Emre", "Nathan", "Anna", "Norman"};
+
+        String[] names2 = new String[names.length];
+        int count = 0;
+
+        for (String each : names) {
+            if (each.substring(0, 1).equalsIgnoreCase(each.substring(each.length() - 1))) {
+                names2[count] = each;
+                count++;
+            }
+        }
+        names2 = Arrays.copyOf(names2, count);
+        System.out.println(count+ " names starts and ends with the same character.");
+        System.out.println("Here is the new array");
+        System.out.println(Arrays.toString(names2));
+    }
 }
