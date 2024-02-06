a7d49444ae424f04110512eeeaadbadf8a0e8e55
European01 <emiryaziciofficial@gmail.com>
2024-02-06 00:14:02 -0500

    Add an example to practice.

--- src/lesson02_ArrayList/practices/RemoveCountryNames.java
+++ src/lesson02_ArrayList/practices/RemoveCountryNames.java
@@ -1,4 +1,19 @@
 package lesson02_ArrayList.practices;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RemoveCountryNames {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> countryNames = new ArrayList<>(Arrays.asList("Saudi Arabia","United Arab Emirates","Israel","Palestine","Lebanon","Jordan"));
+        countryNames.removeIf(country-> country.length() >= 10);
+        System.out.println(countryNames);
+    }
 }
+/*
+6. Create an ArrayList of string called country names, write a program that can remove all the country names that have length of 10 or greater
+
+ */
+
