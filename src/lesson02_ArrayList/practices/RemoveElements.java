e350863e90eb9a144f73eecde4a7a26f7ce4829b
European01 <emiryaziciofficial@gmail.com>
2024-02-06 00:07:37 -0500

    Add an example to practice.

--- src/lesson02_ArrayList/practices/RemoveElements.java
+++ src/lesson02_ArrayList/practices/RemoveElements.java
@@ -1,4 +1,24 @@
 package lesson02_ArrayList.practices;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RemoveElements {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>(Arrays.asList("John", "Ahmed", "Daniel", "Ahmed", "James", "Muhammed"));
+        names.removeAll(Arrays.asList("Ahmed"));
+        System.out.println(names);
+    }
 }
+/*
+Write a program that can remove all the names "Ahmed" from an arraylist of String
+			ex:
+                list = ["John", "Ahmed", "Daniel", "Ahmed", "James", "Muhammed"];
+
+            output:
+            	["John", "Daniel", "James", "Muhammed"];
+
+ */
+
