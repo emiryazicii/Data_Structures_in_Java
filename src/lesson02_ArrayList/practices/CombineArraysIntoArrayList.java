588ae675070c0a6c52fe9b144c5894d01dbb06d1
European01 <emiryaziciofficial@gmail.com>
2024-02-05 23:55:57 -0500

    Add an example to practice.

--- src/lesson02_ArrayList/practices/CombineArraysIntoArrayList.java
+++ src/lesson02_ArrayList/practices/CombineArraysIntoArrayList.java
@@ -1,4 +1,36 @@
 package lesson02_ArrayList.practices;
 
+import java.util.ArrayList;
+
 public class CombineArraysIntoArrayList {
+
+    public static void main(String[] args) {
+
+        String[] arr1 = {"A", "B", "C"};
+        String[] arr2 = {"D", "E", "F", "G"};
+
+        ArrayList<String> list = combine2ArraysToList(arr1, arr2);
+
+        System.out.println(list);
+    }
+    public static ArrayList<String> combine2ArraysToList(String[] arr1, String[] arr2) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (String each : arr1) {
+            list.add(each);
+        }
+        for (String each : arr2) {
+            list.add(each);
+        }
+        return list;
+    }
 }
+/*
+3. write a program that can combine two String arrays into one arrayList
+                ex:
+                    arr1 = {"A", "B", "C"};
+                    arr2 = {"D", "E", "F", "G"};
+                    list ==> {"A", "B", "C", "D", "E", "F", "G"}
+ */
+
