0f237e89b111e8b315d4e0214d004105ab455c91
European01 <emiryaziciofficial@gmail.com>
2024-02-06 00:01:22 -0500

    Add an example to practice.

--- src/lesson02_ArrayList/practices/DuplicateElements.java
+++ src/lesson02_ArrayList/practices/DuplicateElements.java
@@ -1,4 +1,23 @@
 package lesson02_ArrayList.practices;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DuplicateElements {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        numbers.addAll(numbers);
+        System.out.println(numbers);
+    }
 }
+/*
+3. Write a program that can duplicate the elements of an arraylist
+		 	ex:
+                list = [1,2,3,4,5];
+
+            output:
+            	[1,2,3,4,5,1,2,3,4,5];
+ */
+
