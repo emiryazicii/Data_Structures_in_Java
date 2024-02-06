c61ce3802f04bea520893e1c49c7e3691362924d
European01 <emiryaziciofficial@gmail.com>
2024-02-05 21:53:23 -0500

    Add an example to practice.

--- src/lesson01_Array/practices/Items2.java
+++ src/lesson01_Array/practices/Items2.java
@@ -1,4 +1,30 @@
 package lesson01_Array.practices;
 
 public class Items2 {
+
+    public static void main(String[] args) {
+
+        String[][] items = {
+                {"Apple", "Banana", "Grape", "Avocado"},
+                {"Paper Towels", "Toilet Papers", "Tissues", "Diapers"},
+                {"Coke", "Fanta", "Arizona Tea", "Pepsi", "Water"}
+        };
+        String str = "";
+        for (String[] itemsList : items) {
+            for (String eachItem : itemsList) {
+                str += eachItem + " \t";
+            }
+            str = str.trim()+"\n";
+        }
+        System.out.println(str);
+    }
 }
+
+/*
+Given the Array:
+		1. print the following output: (add \t between two words)
+				Apple    Banana   Grape    Avocado
+				Paper Towels     Toilet Papers   Tissues    Diapers
+				Coke   Fanta   Arizona Tea   Pepsi   Water
+ */
+
