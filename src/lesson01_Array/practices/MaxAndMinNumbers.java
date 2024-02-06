08fd0702c60ae7c0f41eb59f97de269ff3501473
European01 <emiryaziciofficial@gmail.com>
2024-02-05 23:20:12 -0500

    Add an example to practice.

--- src/lesson01_Array/practices/MaxAndMinNumbers.java
+++ src/lesson01_Array/practices/MaxAndMinNumbers.java
@@ -1,4 +1,45 @@
 package lesson01_Array.practices;
 
+import java.util.ArrayList;
+
 public class MaxAndMinNumbers {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1,j = 5; i <=5 ; i++, j--) {
+            list.add(i);
+            list.add(j);
+        }
+        System.out.println("Here is the array list : ");
+        System.out.println(list);
+        displayMaxAndMinNumbers(list);
+    }
+    public static void displayMaxAndMinNumbers(ArrayList<Integer> list){
+
+        Integer max = list.get(0) , min = list.get(0);
+
+        for (Integer each : list) {
+            if (each > max){
+                max = each;
+            }
+            if (each < min){
+                min = each;
+            }
+        }
+        System.out.println("Maximum number is: "+max);
+        System.out.println("Minimum number is: "+min);
+    }
 }
+/*
+4. Write a program that can find the maximum & minimum numbers from an ArrayList of integers
+        Ex:
+            list = [1,2,3,4,5];
+
+            output:
+                Maximum number is 5
+                Minimum number is 1
+
+ */
+
